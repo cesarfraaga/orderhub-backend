e42d786c6b4b0723f11f5c8736cb26e1e89f7618
César Fraga <cesarfraaga@gmail.com>
2026-01-06 18:44:13 -0300

    first commit

---
+++ src/main/java/com/core/orderhub_backend/OrderhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.core.orderhub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderhubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/orderhub_backend/OrderhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.orderhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
