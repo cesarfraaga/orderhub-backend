8092f9f255e86da9e3f131f940c26e14d7c584af
César Fraga <cesarfraaga@gmail.com>
2026-01-07 20:50:07 -0300

    feat: add Client and Product entities

---
+++ src/main/java/com/core/orderhub_backend/entity/Client.java
@@ -0,0 +1,26 @@
+package com.core.orderhub_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "cpf", unique = true, nullable = false)
+    private String cpf;
+
+}
---
+++ src/main/java/com/core/orderhub_backend/entity/Product.java
@@ -0,0 +1,29 @@
+package com.core.orderhub_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+}
