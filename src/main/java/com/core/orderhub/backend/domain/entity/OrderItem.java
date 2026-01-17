50dc70b5f191e0d2455b28c7143a91244fec3b2d
César Fraga <cesarfraaga@gmail.com>
2026-01-17 10:20:05 -0300

    chore: add Lombok @Getter and @Setter annotations

--- src/main/java/com/core/orderhub/backend/domain/entity/OrderItem.java
+++ src/main/java/com/core/orderhub/backend/domain/entity/OrderItem.java
@@ -1,9 +1,13 @@
 package com.core.orderhub.backend.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 @Entity
 public class OrderItem {
 
