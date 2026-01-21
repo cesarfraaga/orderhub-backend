9a0cff485df7330f968f8f37fbd900f07a02fef3
César Fraga <cesarfraaga@gmail.com>
2026-01-21 10:32:17 -0300

    feat: model OrderStatus enum as a state machine

--- src/main/java/com/core/orderhub/backend/domain/enums/OrderStatus.java
+++ src/main/java/com/core/orderhub/backend/domain/enums/OrderStatus.java
@@ -1,9 +1,34 @@
 package com.core.orderhub.backend.domain.enums;
 
 public enum OrderStatus {
-    CREATED,
-    PAID,
-    SENT,
-    FINISHED,
-    CANCELED
+
+    CREATED {
+        @Override
+        public boolean canTransitionTo(OrderStatus newStatus) {
+            return newStatus == PAID || newStatus == CANCELED;
+        }
+    },
+
+    PAID {
+        @Override
+        public boolean canTransitionTo(OrderStatus newStatus) {
+            return newStatus == FINISHED || newStatus == CANCELED;
+        }
+    },
+
+    FINISHED {
+        @Override
+        public boolean canTransitionTo(OrderStatus newStatus) {
+            return false;
+        }
+    },
+
+    CANCELED {
+        @Override
+        public boolean canTransitionTo(OrderStatus newStatus) {
+            return false;
+        }
+    };
+
+    public abstract boolean canTransitionTo(OrderStatus newStatus);
 }
