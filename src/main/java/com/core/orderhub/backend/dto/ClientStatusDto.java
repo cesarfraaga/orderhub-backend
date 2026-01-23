d46fd4372a01a40a296fae450b49642acf154845
César Fraga <cesarfraaga@gmail.com>
2026-01-23 10:15:35 -0300

    feat: add Bean Validation to Product and Client status DTOs

--- src/main/java/com/core/orderhub/backend/dto/ClientStatusDto.java
+++ src/main/java/com/core/orderhub/backend/dto/ClientStatusDto.java
@@ -1,11 +1,13 @@
 package com.core.orderhub.backend.dto;
 
 import com.core.orderhub.backend.domain.enums.ClientStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ClientStatusDto {
 
+    @NotNull(message = "client status cannot be null.")
     private ClientStatus status;
 
 }
--- src/main/java/com/core/orderhub/backend/dto/ProductStatusDto.java
+++ src/main/java/com/core/orderhub/backend/dto/ProductStatusDto.java
@@ -1,11 +1,13 @@
 package com.core.orderhub.backend.dto;
 
 import com.core.orderhub.backend.domain.enums.ProductStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ProductStatusDto {
 
+    @NotNull(message = "product status cannot be null.")
     private ProductStatus status;
 
 }
