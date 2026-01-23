00319e83de64472db2d88be3a939cac7cb847972
César Fraga <cesarfraaga@gmail.com>
2026-01-22 22:13:40 -0300

    refactor: move client validations to Bean Validation

--- src/main/java/com/core/orderhub/backend/controller/ClientController.java
+++ src/main/java/com/core/orderhub/backend/controller/ClientController.java
@@ -3,6 +3,8 @@ package com.core.orderhub.backend.controller;
 import com.core.orderhub.backend.dto.ClientDto;
 import com.core.orderhub.backend.dto.ClientStatusDto;
 import com.core.orderhub.backend.service.ClientService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,44 +19,41 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
-    @PostMapping("/save")
-    public ResponseEntity<ClientDto> save(@RequestBody ClientDto clientDto) {
+    @PostMapping
+    public ResponseEntity<ClientDto> save(@RequestBody @Valid ClientDto clientDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(clientService.createClient(clientDto));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ClientDto> update(
-            @PathVariable Long id, //+ legibilidade
-            @RequestBody ClientDto clientDto
-    ) { //Em REST, preciso identificar o recurso que vai ser att
+            @PathVariable @Positive Long id,
+            @Valid @RequestBody ClientDto clientDto
+    ) {
         return ResponseEntity.status(HttpStatus.OK).body(clientService.updateClient(id, clientDto));
     }
 
     @PatchMapping("/{id}/status")
     public ResponseEntity<Void> updateStatus(
-            @PathVariable Long id,
-            @RequestBody ClientStatusDto dto
+            @PathVariable @Positive Long id,
+            @Valid @RequestBody ClientStatusDto dto
     ) {
         clientService.updateClientStatus(id, dto.getStatus());
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ClientDto> findById(@PathVariable Long id) {
+    public ResponseEntity<ClientDto> findById(@PathVariable @Positive Long id) {
         ClientDto clientDto = clientService.findById(id);
-        if (clientDto == null) {
-            return ResponseEntity.notFound().build();
-        }
         return ResponseEntity.ok(clientDto);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable @Positive Long id) {
         clientService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("findAll")
+    @GetMapping
     public ResponseEntity<List<ClientDto>> findAll() {
         List<ClientDto> clientDtoList = clientService.findAll(); //Melhor alinhar com o que fiz na order, pra deixar mais enxuto
         return ResponseEntity.ok(clientDtoList);
--- src/main/java/com/core/orderhub/backend/dto/ClientDto.java
+++ src/main/java/com/core/orderhub/backend/dto/ClientDto.java
@@ -1,6 +1,10 @@
 package com.core.orderhub.backend.dto;
 
 import com.core.orderhub.backend.domain.enums.ClientStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,9 +14,15 @@ public class ClientDto {
 
     private Long id;
 
+    @NotBlank(message = "name cannot be null or empty.")
+    @Size(min = 2, max = 50, message = "client name cannot be less than 2 or more than 50 characters.")
+    @Pattern(regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "client name cannot contain special characters.")
     private String name;
 
+    @NotBlank(message = "cpf cannot be null or empty")
+    @Pattern(regexp = "\\d{11}", message = "CPF must have exactly 11 digits.")
     private String cpf;
 
+    @NotNull(message = "client status cannot be null.")
     private ClientStatus status;
 }
