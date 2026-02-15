4cd82ba750b974d367b6cc58de3f6c26e9e90d9f
César Fraga <cesarfraaga@gmail.com>
2026-02-15 20:58:31 -0300

    feat: add existsByCpf to ClientRepository

--- src/main/java/com/core/orderhub/backend/repository/ClientRepository.java
+++ src/main/java/com/core/orderhub/backend/repository/ClientRepository.java
@@ -4,4 +4,5 @@ import com.core.orderhub.backend.domain.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    boolean existsByCpf(String cpf);
 }
