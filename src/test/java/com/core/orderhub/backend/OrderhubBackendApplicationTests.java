248777bc5e88d9f6afe3e09f85ee59d3afc46df6
César Fraga <cesarfraaga@gmail.com>
2026-02-16 14:44:11 -0300

    test: add assertion to contextLoads

--- src/test/java/com/core/orderhub/backend/OrderhubBackendApplicationTests.java
+++ src/test/java/com/core/orderhub/backend/OrderhubBackendApplicationTests.java
@@ -1,13 +1,20 @@
 package com.core.orderhub.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
 @SpringBootTest
 class OrderhubBackendApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
 	}
-
 }
