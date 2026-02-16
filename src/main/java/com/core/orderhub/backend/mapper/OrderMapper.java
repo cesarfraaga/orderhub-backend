8d3e096dee730a48bd445be1eb7b8ec5415ee70e
César Fraga <cesarfraaga@gmail.com>
2026-02-16 20:58:54 -0300

    feat: add OrderItemMapper

--- src/main/java/com/core/orderhub/backend/mapper/OrderMapper.java
+++ src/main/java/com/core/orderhub/backend/mapper/OrderMapper.java
@@ -3,10 +3,13 @@ package com.core.orderhub.backend.mapper;
 import com.core.orderhub.backend.dto.OrderDto;
 import com.core.orderhub.backend.domain.entity.Order;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = OrderItemMapper.class)
 public interface OrderMapper {
 
+    @Mapping(source = "client.id", target = "clientId")
+    @Mapping(source = "orderItemList", target = "items")
     OrderDto toDto(Order order);
 
     Order toEntity(OrderDto orderDto);
