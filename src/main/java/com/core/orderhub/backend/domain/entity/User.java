e6b279be60977770960d408d74d4497fd59f3d1a
César Fraga <cesarfraaga@gmail.com>
2026-04-10 13:13:34 -0300

    feat: add User entity

---
+++ src/main/java/com/core/orderhub/backend/domain/entity/User.java
@@ -0,0 +1,56 @@
+package com.core.orderhub.backend.domain.entity;
+
+import com.core.orderhub.backend.domain.enums.UserRoles;
+import com.core.orderhub.backend.domain.enums.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+    @Enumerated(EnumType.STRING)
+    private Set<UserRoles> roles;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.userStatus = UserStatus.ACTIVE;
+        this.roles = new HashSet<>();
+        this.roles.add(UserRoles.ROLE_USER);
+    }
+
+    public void changeStatus(UserStatus newStatus) {
+        this.userStatus = newStatus;
+    }
+
+}
