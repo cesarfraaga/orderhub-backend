c65de2846302c34fe8ba71bbca0d7bbc9d02c98b
César Fraga <cesarfraaga@gmail.com>
2026-04-14 12:31:39 -0300

    feat: add JwtService class

---
+++ src/main/java/com/core/orderhub/backend/service/JwtService.java
@@ -0,0 +1,32 @@
+package com.core.orderhub.backend.service;
+
+import com.core.orderhub.backend.domain.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "ZmFrZVNlY3JldEtleUZvckp3dFRva2VuMTIzNDU2"; //TODO: usar variáveis de ambiente
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("roles", user.getRoles())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
