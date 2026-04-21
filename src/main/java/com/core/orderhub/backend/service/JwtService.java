1bc65778ac7db168596aa102590c6faf8b75d2da
César Fraga <cesarfraaga@gmail.com>
2026-04-21 12:13:11 -0300

    feat: add JWT token validation and externalize secret key

--- src/main/java/com/core/orderhub/backend/service/JwtService.java
+++ src/main/java/com/core/orderhub/backend/service/JwtService.java
@@ -1,10 +1,12 @@
 package com.core.orderhub.backend.service;
 
 import com.core.orderhub.backend.domain.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -13,20 +15,33 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private final String SECRET_KEY = "ZmFrZVNlY3JldEtleUZvckp3dFRva2VuMTIzNDU2"; //TODO: usar variáveis de ambiente
+    @Value("${api.security.token.secret}")
+    private String secret;
 
     public String generateToken(User user) {
         return Jwts.builder()
                 .setSubject(user.getEmail())
-                .claim("roles", user.getRoles())
+                .claim("roles", user.getRoles().stream()
+                        .map(Enum::name)
+                        .toList())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
+    public String validateToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
     private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
